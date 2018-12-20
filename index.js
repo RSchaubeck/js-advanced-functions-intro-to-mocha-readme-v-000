5e0e78a2cba03d73b1500424a76652eeff8a67f3
Richard Schaubeck <richard.schaubeck@gmail.com>
2018-12-20 20:14:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true;
+}
+
+function sayHey() {
+  return "Hey!";
+}
+
+function sayHeyFriend() {
+
+}
+
+function favoriteIceCream() {
+
+}
+
+function shouting() {
+
+}
+
+function roundDown() {
+
+}
